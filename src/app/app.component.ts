fc16d2d1422288d1ca286802636b5be9c9012997
Ayan <ayanghosh1389@gmail.com>
2022-02-17 21:57:22 +0530

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { applySourceSpanToExpressionIfNeeded } from '@angular/compiler/src/output/output_ast';
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+
+}  
